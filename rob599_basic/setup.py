ebac7f5cbd0e8c63bbca0187dc755d3a46e14cc0
Bill Smart <bill.smart@oregonstate.edu>
2024-02-11 22:50:01 -0800

    Initial commit of new files and structure.

---
+++ rob599_basic/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+package_name = 'rob599_basic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Bill Smart',
+    maintainer_email='smartw@oregonstate.edu',
+    description='ROB599 ROS 2 Examples',
+    license='BSD 3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'publisher = rob599_basic.publisher:main',
+            'subscriber = rob599_basic.subscriber:main',
+            'service = rob599_basic.service:main',
+            'client = rob599_basic.service_client:main',
+            'action_server = rob599_basic.action_server:main',
+            'action_client = rob599_basic.action_client:main',
+        ],
+    },
+)
